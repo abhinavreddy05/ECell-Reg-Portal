826fd75282415f4b7c836994de985411abcb42b9
abhinavreddy05 <abhinavreddy05@kgpian.iitkgp.ac.in>
2023-06-25 19:04:46 +0530

    landing page completed

--- reg_portal/landingPage/urls.py
+++ reg_portal/landingPage/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('about', views.about, name="about"),
     path('contact', views.contact, name="contact"),
 ]

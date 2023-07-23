2278346a9eb374a85c597beca4e1c24f5229d2b8
G Abhinav Reddy <117709452+abhinavreddy05@users.noreply.github.com>
2023-07-23 05:00:04 +0000

    .

--- reg_portal/ead/urls.py
+++ reg_portal/ead/urls.py
@@ -7,12 +7,13 @@ from .forms import CustomAuthenticationForm
 urlpatterns = [
     path('', views.register, name="ead_home"),
     path('register/', views.register, name="ead_register"),
+    path('register/r/', views.register),
     path('register/<slug:code>/', views.register, name="ead_register"),
     path('login/',  LoginView.as_view(template_name='ead/login.html', authentication_form=CustomAuthenticationForm), name="ead_login"),
 
     path('dashboard/', views.dashboard, name="ead_dashboard"),
-    path('result/', views.result, name="ead_result"),
-    path('event/', views.event, name="ead_event"),
+    path('result/', views.certificate, name="ead_result"),
+    path('event/', views.certificate, name="ead_event"),
     path('certificate/', views.certificate, name="ead_certificate"),
 
     path('logout/', LogoutView.as_view(template_name='customUser/mainpage.html'), name='logout'),
